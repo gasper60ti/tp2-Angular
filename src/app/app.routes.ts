62bf14adeeae76cb3e109aa4a175ebdc6634df42
gasper60ti <kn_fliti@esi.dz>
2024-02-26 11:15:11 +0100

    tp2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { C1Component } from './c1/c1.component';
+import { C2Component } from './c2/c2.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+    { path: "comp1", component: C1Component },
+    { path: "comp2", component: C2Component },
+    { path: "", redirectTo: "/home", pathMatch: "full" },
+    { path: "home", component: HomeComponent },
+    
+    // { path: "**", component: NotFoundComponent},
+    { path: "404error", component: NotFoundComponent},
+    { path: "**", redirectTo: "/404error", pathMatch: "full"}
+];
